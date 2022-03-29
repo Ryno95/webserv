b44b0c8b5ade3e585a6ba2355a854633ec5251ca
Ryno Meiboom <rmeiboom@f1r5s11.codam.nl>
2022-03-29 17:30:03 +0200

    refactored

--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -5,7 +5,6 @@
 
 Request::Request(std::string query) : _query(query)
 {
-	// std::cout << query << std::endl;
 	(void)_method;
 	parse();
 }
@@ -24,11 +23,6 @@ httpStatusCode Request::parseRequestLine()
 {
 	return OK;
 }
-
-
-// User-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3
-// Host: www.example.com
-// Accept-Language: en, mi 
 
 httpStatusCode	addKeyValuePair(const std::string &src, size_t newLinePos,
 						std::map<std::string, std::string>*headerFields)
