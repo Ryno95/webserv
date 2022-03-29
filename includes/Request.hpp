84259acbe7959de44fd69e9ab5490e27ad02a664
pspijkst <pspijkst@student.codam.nl>
2022-03-29 11:29:08 +0200

    Set up request structure

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -1,16 +1,29 @@
 #pragma once
 
 #include <string>
+#include <map>
+
+#include <defines.hpp>
 
 class Request
 {
 public:
-	Request(std::string uri);
+	Request(std::string request);
 	~Request();
 
 private:
-	void parseURI();
+	void parse();
+	void parseBody();
+	void parseRequestLine();
+	void parseHeaderFields();
 
-	std::string _uri;
+
+	std::string	_request;
+
+	method		_method;
+	std::string	_target;
+	std::string	_version;
+	std::map<std::string, std::string>	_headerFields;
+	std::string	_body;
 
 };
--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -1,13 +1,27 @@
 #include <Request.hpp>
 
-Request::Request(std::string uri) : _uri(uri)
+Request::Request(std::string request) : _request(request)
 {
+	(void)_method;
 }
 
 Request::~Request()
 {
 }
 
-void Request::parseURI()
+void Request::parseBody()
+{
+}
+
+// request-line = method SP request-target SP HTTP-version CRLF
+void Request::parseRequestLine()
+{
+}
+
+void Request::parseHeaderFields()
+{
+}
+
+void Request::parse()
 {
 }
