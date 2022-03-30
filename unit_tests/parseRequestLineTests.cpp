1e8202495c3ff7219e088f4bfa471c0f8deea6f3
pspijkst <pspijkst@student.codam.nl>
2022-03-30 16:44:16 +0200

    refactor to match new program structure

---
+++ unit_tests/parseRequestLineTests.cpp
@@ -0,0 +1,420 @@
+#include <criterion/criterion.h>
+#include <Request.hpp>
+
+Test(simple, valid)
+{
+	cr_expect(1 == 1);
+}
+
+Test(requestLineTests, INVALID_METHOD)
+{
+	std::string seed = "BLADIEBLA / HTTP/1.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, GET_valid_request)
+{
+	std::string seed = "GET / HTTP/1.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request._method == GET);
+	cr_expect(request._target == "/");
+	cr_expect(request.getStatus() == OK);
+}
+
+Test(requestLineTests, GET_valid_request_large_path)
+{
+	std::string seed = "GET /fjadsffkjadskljffffjadsffkjadskljfffdsfadsjfjdjsfkldjfadjslfkjdsfadsjfjdjsfkldjfadjslfkj HTTP/1.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	if (request.getStatus() == URI_TOO_LONG)
+		return;
+	cr_expect(request.getStatus() == OK);
+	cr_expect(request._method == GET);
+	cr_expect(request._target == "/fjadsffkjadskljffffjadsffkjadskljfffdsfadsjfjdjsfkldjfadjslfkjdsfadsjfjdjsfkldjfadjslfkj");
+}
+
+Test(requestLineTests, POST_valid_request)
+{
+	std::string seed = "POST / HTTP/1.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == OK);
+	cr_expect(request._method == POST);
+	cr_expect(request._target == "/");
+}
+
+Test(requestLineTests, DELETE_valid_request)
+{
+	std::string seed = "DELETE / HTTP/1.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == OK);
+	cr_expect(request._method == DELETE);
+	cr_expect(request._target == "/");
+}
+
+Test(requestLineTests, DELETE_no_target)
+{
+	std::string seed = "DELETE  HTTP/1.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, DELETE_no_version)
+{
+	std::string seed = "DELETE / \r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, DELETE_no_version2)
+{
+	std::string seed = "DELETE /\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, DELETE_too_many_whitespace1)
+{
+	std::string seed = "DELETE  / HTTP/1.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, DELETE_too_many_whitespace2)
+{
+	std::string seed = "DELETE /  HTTP/1.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, empty_seed)
+{
+	std::string seed;
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, DELETE_leading_whitespace)
+{
+	std::string seed = " DELETE / HTTP/1.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, DELETE_trailing_whitespace)
+{
+	std::string seed = "DELETE / HTTP/1.1 \r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, target_just_fits)
+{
+	std::string seed = "GET ";
+	for (size_t i = 0; i < MAX_TARGET_LEN; ++i)
+	{
+		seed += "a";
+	}
+	seed += " HTTP/1.1\r\n";
+	
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == OK);
+}
+
+
+Test(requestLineTests, target_just_too_long)
+{
+	std::string seed = "GET ";
+	for (size_t i = 0; i <= MAX_TARGET_LEN; ++i)
+	{
+		seed += "a";
+	}
+	seed += " HTTP/1.1\r\n";
+	
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == URI_TOO_LONG);
+}
+
+Test(requestLineTests, GET_invalid_version1)
+{
+	std::string seed = "GET / HTTP/a1.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, GET_invalid_version2)
+{
+	std::string seed = "GET / HTTP/1a.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, GET_invalid_version3)
+{
+	std::string seed = "GET / HTTP/1.a1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, GET_invalid_version4)
+{
+	std::string seed = "GET / HTTP/1.1a\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, GET_invalid_version5)
+{
+	std::string seed = "GET / HTTP/1.\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, GET_invalid_version6)
+{
+	std::string seed = "GET / HTTP/.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, GET_redundant_zeros)
+{
+	std::string seed = "GET / HTTP/00000001.000000000000001\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == OK);
+}
+
+Test(requestLineTests, GET_CR_in_target)
+{
+	std::string seed = "GET /\r HTTP/1.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, GET_LF_in_target)
+{
+	std::string seed = "GET /\n HTTP/1.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, GET_CRLF_in_target)
+{
+	std::string seed = "GET /\r\n HTTP/1.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
+
+Test(requestLineTests, GET_LFCR_in_target)
+{
+	std::string seed = "GET /\n\r HTTP/1.1\r\n";
+	Request request(seed);
+	try
+	{
+		request.parseRequestLine();
+	}
+	catch(const std::exception& e)
+	{
+	}
+
+	cr_expect(request.getStatus() == BAD_REQUEST);
+}
