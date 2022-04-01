07a0a3f4a8e32c68d9971d9e0371ae242e85ea57
Ryno Meiboom <rmeiboom@f0r5s1.codam.nl>
2022-04-01 15:54:18 +0200

    trying to add python

--- acceptence_tests/acceptence.sh
+++ acceptence_tests/acceptence.sh
@@ -5,7 +5,7 @@
 # Save the PID to kill the webserv
 PID=$!
 # sleep for 1 second to give the server time to start-up
-sleep 1
+sleep 3
 # run the tests
 python3 acceptence_tests/TestClient.py 
 # save the return val of the tests foor actions
