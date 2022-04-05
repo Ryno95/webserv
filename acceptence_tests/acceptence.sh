fbab5a46da0c7d55d3fdebab70c5b8938f71d5f8
Rynoster <Ryno.Meiboom@gmail.com>
2022-04-05 22:06:35 +0200

    trying curl with a body

--- acceptence_tests/acceptence.sh
+++ acceptence_tests/acceptence.sh
@@ -5,10 +5,15 @@
 # Save the PID to kill the webserv
 PID=$!
 # sleep for 1 second to give the server time to start-up
-sleep 3
+sleep 2
 # run the tests
 
-curl -v localhost:8080
+# curl -v localhost:8080
+curl -X GET \
+  -H "Content-type: application/json" \
+  -H "Accept: application/json" \
+  -d '{"param0":"pradeep"}' \
+  "http://localhost:8080"
 # python3 acceptence_tests/TestClient.py
 # # save the return val of the tests foor actions
 T1=$?
