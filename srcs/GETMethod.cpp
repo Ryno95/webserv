bda85c57cf4c7e051754fcaaed61df6c9e9698d0
pspijkst <pspijkst@student.codam.nl>
2022-04-14 15:15:33 +0200

    Basic GET processing added

--- srcs/GETMethod.cpp
+++ srcs/GETMethod.cpp
@@ -8,9 +8,26 @@ GETMethod::~GETMethod()
 {
 }
 
+std::string createFilePath(std::string path)
+{
+	path.erase(0, 1);
+	return path;
+}
+
 Response* GETMethod::process()
 {
+	std::ifstream* stream = new std::ifstream();
+
+	stream->open(createFilePath(_request.getTarget()));
+	if (stream->fail())
+	{
+		delete stream;
+		_response->setStatusCode(HttpStatusCodes::NOT_FOUND);
+		return _response;
+	}
+
+	_response->setBodyStream(stream);
 	_response->setStatusCode(HttpStatusCodes::OK);
-	setInputFile(_request.getTarget());
+
 	return _response;
 }
