4e7a282ab626f3b868caf0c286fbe5865ec10184
pspijkst <pspijkst@student.codam.nl>
2022-04-14 15:14:31 +0200

    Changed namings

--- includes/HttpStatusCode.hpp
+++ includes/HttpStatusCode.hpp
@@ -7,9 +7,9 @@ typedef std::pair<int, std::string> HttpStatusCode;
 namespace HttpStatusCodes
 {
 	const HttpStatusCode OK					= HttpStatusCode(200, "OK");
-	const HttpStatusCode BAD_REQUEST		= HttpStatusCode(400, "BAD_REQUEST");
-	const HttpStatusCode NOT_FOUND			= HttpStatusCode(404, "NOT_FOUND");
-	const HttpStatusCode URI_TOO_LONG		= HttpStatusCode(414, "URI_TOO_LONG");
-	const HttpStatusCode TEAPOT				= HttpStatusCode(418, "TEAPOT");
-	const HttpStatusCode NOT_IMPLEMENTED	= HttpStatusCode(501, "NOT_IMPLEMENTED");
+	const HttpStatusCode BAD_REQUEST		= HttpStatusCode(400, "Bad Request");
+	const HttpStatusCode NOT_FOUND			= HttpStatusCode(404, "Not Found");
+	const HttpStatusCode URI_TOO_LONG		= HttpStatusCode(414, "Request-URI Too Long");
+	const HttpStatusCode TEAPOT				= HttpStatusCode(418, "Teapot");
+	const HttpStatusCode NOT_IMPLEMENTED	= HttpStatusCode(501, "Not Implemented");
 }
