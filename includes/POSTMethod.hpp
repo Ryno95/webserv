91aa67076592154df357a78906407870b5088d79
Ryno Meiboom <rmeiboom@f0r5s2.codam.nl>
2022-04-15 15:07:41 +0200

    added POSTMethod files and class

---
+++ includes/POSTMethod.hpp
@@ -0,0 +1,10 @@
+#include <AMethod.hpp>
+#include <Request.hpp>
+#include <Response.hpp>
+
+class POSTMethod : public AMethod
+{
+    public:
+        POSTMethod(const Request &request);
+        Response *process();
+};
