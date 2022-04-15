d6e652aa2cd854a4d357de183a7798929302cbba
Ryno Meiboom <rmeiboom@f0r5s2.codam.nl>
2022-04-15 15:08:21 +0200

    Added comments for POST implementation

--- includes/AMethod.hpp
+++ includes/AMethod.hpp
@@ -22,6 +22,16 @@ Methods:
 		- with body
 		- execute file at target URI
 		- returns result
+
+		1. Open file or create file
+		2. Append to file
+		3. 
+
+		// Parsing a POST request
+		If you are sending data with your POST request, you must provide the Content-Type and Content-Length HTTP headers that indicate the type and size of the data in your POST message.
+		These HTTP headers will help the server interpret and process the sent data correctly.
+		Alternatively, you can omit the Content-Length header for your POST request and use the Transfer-Encoding header instead.
+		If the Content-Length and Transfer-Encoding headers are missing, the connection MUST be closed at the end of the response.
 	
 	DELETE:
 		- optional body, mostly not
