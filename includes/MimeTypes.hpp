332df046592465c900b65a7d937c15659be36272
Pascal Spijkstra <pascalspijkstra@gmail.com>
2022-04-20 11:06:02 +0200

    MimeTypes class initial create

---
+++ includes/MimeTypes.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <map>
+
+class MimeTypes
+{
+public:
+	MimeTypes();
+	MimeTypes(const std::string& filePath);
+	~MimeTypes();
+
+	const std::string& getMIMEType(const std::string& extension) const;
+
+private:
+	void parse();
+
+	std::map<const std::string, const std::string> _types;
+	std::string _filePath;
+
+};
