123305aca89619c4e0ace483b57dd9f62e33c5ba
Pascal Spijkstra <pascalspijkstra@gmail.com>
2022-04-20 14:02:22 +0200

    Support for macro's DEBUG and WARN added

--- srcs/Logger.cpp
+++ srcs/Logger.cpp
@@ -2,6 +2,7 @@
 #include <Colors.hpp>
 
 #include <ctime>
+#include <sstream>
 
 std::ofstream Logger::_logFile = std::ofstream(LOGFILE);
 
@@ -35,6 +36,9 @@ static void printDebug(const std::string& msg)
 
 void Logger::log(const std::string& msg)
 {
+	if (!_enableLogging)
+		return;
+
 	if (!_logFile.is_open())
 	{
 		printWarning("There's no logfile, no logs will be saved.");
@@ -46,6 +50,7 @@ void Logger::log(const std::string& msg)
 	_logFile.write(timestamp.c_str(), timestamp.size());
 	_logFile.write(msg.c_str(), msg.size());
 	_logFile.write("\n", 1);
+	_logFile.flush();
 }
 
 void Logger::warn(const std::string& msg)
@@ -64,8 +69,14 @@ void Logger::debug(const std::string& msg)
 		log("[DEBUG] " + msg);
 }
 
-int main()
+void Logger::warn(const std::ostream& s)
 {
-	Logger::debug("Hello world!");
-	Logger::warn("THIS IS GOOOOD");
+	const std::stringstream& ss = static_cast<const std::stringstream&>(s);
+	Logger::warn(ss.str());
+}
+
+void Logger::debug(const std::ostream& s)
+{
+	const std::stringstream& ss = static_cast<const std::stringstream&>(s);
+	Logger::debug(ss.str());
 }
