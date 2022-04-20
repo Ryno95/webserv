2556c8634897fdd552b2cbc03d35472052e33fb7
Pascal Spijkstra <pascalspijkstra@gmail.com>
2022-04-20 11:06:25 +0200

    Test class for mime initial create

---
+++ unit_tests/parseMimeTypesTests.cpp
@@ -0,0 +1,30 @@
+#include <criterion/criterion.h>
+#include <MimeTypes.hpp>
+
+Test(Mimes, valid)
+{
+	MimeTypes mimes("config/TESTmime.config");
+
+	cr_expect(mimes.getMIMEType("html") == "text/html");
+	cr_expect(mimes.getMIMEType("htm") == "text/html");
+	cr_expect(mimes.getMIMEType("shtml") == "text/html");
+	cr_expect(mimes.getMIMEType("mng") == "video/x-mng");
+}
+
+Test(Mimes, invalid)
+{
+	MimeTypes mimes("config/TESTmime.config");
+
+	bool boolean;
+	try
+	{
+		mimes.getMIMEType("doesnt/exist");
+		boolean = true;
+	}
+	catch(const std::exception& e)
+	{
+		boolean = false;
+	}
+
+	cr_expect(boolean == false);
+}
