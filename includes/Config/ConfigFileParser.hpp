d6665865c75e6c4432c4bbb2c76aa562a636075c
pspijkst <pspijkst@student.codam.nl>
2022-05-03 19:06:52 +0200

    Created the parser in a dynamic way :)

--- includes/Config/ConfigFileParser.hpp
+++ includes/Config/ConfigFileParser.hpp
@@ -1,9 +1,35 @@
 #pragma once
 
 #include <string>
+#include <map>
 #include <vector>
+#include <iostream>
 
-#include <ServerConfig.hpp>
+#include <config/ServerConfig.hpp>
+#include <Logger.hpp>
+
+namespace ServerVariables
+{
+	enum vars
+	{
+		LISTEN_PORT,
+		BUFFER_SIZE,
+		MIME_CONFIG,
+		LISTEN_BACKLOG
+	};
+}
+
+namespace HostVariables
+{
+	enum vars
+	{
+		NAME,
+		ROOT,
+		METHODS,
+		DEFAULT_INDEX,
+		DEFAULT_ERROR
+	};
+}
 
 class ConfigFileParser
 {
@@ -14,10 +40,92 @@ public:
 	std::vector<ServerConfig> parse();
 
 private:
-	HostConfig parseHostConfig() const;
-	ServerConfig parseServerConfig() const;
+	typedef enum
+	{
+		NONE,
+		PRE_SERVER_BLOCK,
+		IN_SERVER_BLOCK,
+		PRE_HOST_BLOCK,
+		IN_HOST_BLOCK,
+	} state;
+
+	template<class T>
+	void parseVariable(const std::string& value, T* dest) const;
+
+	void parseHostVariable(const std::string& line, HostConfig& config) const;
+	void parseServerVariable(const std::string& line, ServerConfig& config) const;
+
+	void fillServerVariablesMap();
+	void fillHostVariablesMap();
+
 
 	std::vector<ServerConfig> _serverConfigs;
 	std::string _filePath;
+	size_t _lineCount;
+	std::map<std::string, int> _hostVariables;
+	std::map<std::string, int> _serverVariables;
 
 };
+
+template<>
+inline void ConfigFileParser::parseVariable(const std::string& value, uint* dest) const
+{
+	*dest = atoi(value.c_str());
+}
+
+template<>
+inline void ConfigFileParser::parseVariable(const std::string& value, std::string* dest) const
+{
+	*dest = value;
+}
+
+std::string removeLeadingWhitespace(const std::string &str);
+
+template<>
+inline void ConfigFileParser::parseVariable(const std::string& value, std::vector<std::string>* dest) const
+{
+	std::string str = value;
+
+	while (true)
+	{
+		size_t end = std::min(std::min(str.find(' '), str.find('\t')), str.size());
+		dest->push_back(str.substr(0, end));
+
+		if (end == str.size())
+			return;
+
+		str = str.substr(end, str.size() - end);
+		str = removeLeadingWhitespace(str);
+	}
+}
+
+template<>
+inline void ConfigFileParser::parseVariable(const std::string& value, std::vector<Method::method>* dest) const
+{
+	std::string str = value;
+	std::string method;
+
+	while (true)
+	{
+		size_t end = std::min(std::min(str.find(' '), str.find('\t')), str.size());
+		method = str.substr(0, end);
+
+		if (method == "GET")
+			dest->push_back(Method::GET);
+		else if (method == "POST")
+			dest->push_back(Method::POST);
+		else if (method == "DELETE")
+			dest->push_back(Method::DELETE);
+		else
+		{
+			ERROR("Unhandled method supplied: " << method);
+			throw std::runtime_error("Unhandled method supplied");
+		}
+
+		if (end == str.size())
+			return;
+
+		str = str.substr(end, str.size() - end);
+		str = removeLeadingWhitespace(str);
+	}
+}
--- srcs/ConfigFileParser.cpp
+++ srcs/ConfigFileParser.cpp
@@ -1,7 +1,54 @@
-#include <ConfigFileParser.hpp>
+#include <fstream>
+#include <sstream>
+#include <map>
 
-ConfigFileParser::ConfigFileParser(const std::string& filePath) : _filePath(filePath)
+#include <config/ConfigFileParser.hpp>
+#include <Logger.hpp>
+
+std::string removeLeadingWhitespace(const std::string &str)
 {
+	std::string out = str;
+	std::string::const_iterator iter = str.begin();
+	std::string::const_iterator end = str.end();
+	size_t count = 0;
+
+	while (iter != end)
+	{
+		if (*iter == ' ' || *iter == '\t')
+			count++;
+		else
+			break;
+		iter++;
+	}
+
+	out.erase(0, count);
+	return out;
+}
+
+std::string removeTrailingWhitespace(const std::string &str)
+{
+	std::string out = str;
+	std::string::const_reverse_iterator iter = str.rbegin();
+	std::string::const_reverse_iterator end = str.rend();
+	size_t count = 0;
+
+	while (iter != end)
+	{
+		if (*iter == ' ' || *iter == '\t')
+			count++;
+		else
+			break;
+		iter++;
+	}
+
+	out.erase(out.size() - count, count);
+	return out;
+}
+
+ConfigFileParser::ConfigFileParser(const std::string &filePath) : _filePath(filePath), _lineCount(0)
+{
+	fillHostVariablesMap();
+	fillServerVariablesMap();
 }
 
 ConfigFileParser::~ConfigFileParser()
@@ -14,4 +61,179 @@ ConfigFileParser::~ConfigFileParser()
 */
 std::vector<ServerConfig> ConfigFileParser::parse()
 {
+	std::ifstream fstream(_filePath);
+	std::string line;
+	state currentState = NONE;
+	ServerConfig currentServerConfig;
+	HostConfig currentHostConfig;
+
+	if (fstream.fail())
+		throw std::runtime_error("Error opening config file");
+
+	while (std::getline(fstream, line))
+	{
+		_lineCount++;
+		line = removeLeadingWhitespace(line);
+		line = removeTrailingWhitespace(line);
+
+		if (line.size() == 0 || line.front() == '#')
+			continue;
+
+		switch (currentState)
+		{
+		case NONE:
+			if (line == "server")
+				currentState = PRE_SERVER_BLOCK;
+			else
+			{
+				ERROR("Invalid input on line [" << _lineCount << "]: " << line);
+				throw std::runtime_error("Parse error");
+			}
+			break;
+
+		case PRE_SERVER_BLOCK:
+			if (line == "{")
+				currentState = IN_SERVER_BLOCK;
+			else
+			{
+				ERROR("Expected '{' on line [" << _lineCount << "]: " << line);
+				throw std::runtime_error("Parse error");
+			}
+			break;
+
+		case IN_SERVER_BLOCK:
+			if (line == "}")
+			{
+				_serverConfigs.push_back(currentServerConfig);
+				std::memset(&currentServerConfig, 0, sizeof(ServerConfig));
+				currentState = NONE;
+			}
+			else if (line == "host")
+				currentState = PRE_HOST_BLOCK;
+			else
+				parseServerVariable(line, currentServerConfig);
+			break;
+
+		case PRE_HOST_BLOCK:
+			if (line == "{")
+				currentState = IN_HOST_BLOCK;
+			else
+			{
+				ERROR("Expected '{' on line [" << _lineCount << "]: " << line);
+				throw std::runtime_error("Parse error");
+			}
+			break;
+
+		case IN_HOST_BLOCK:
+			if (line == "}")
+			{
+				currentServerConfig.hosts.push_back(currentHostConfig);
+				std::memset(&currentHostConfig, 0, sizeof(HostConfig));
+				currentState = IN_SERVER_BLOCK;
+			}
+			else
+				parseHostVariable(line, currentHostConfig);
+			break;
+		}
+	}
+
+	if (currentState != NONE)
+	{
+		ERROR("Unclosed section encountered");
+		throw std::runtime_error("Parse error");
+	}
+
+	return _serverConfigs;
+}
+
+void ConfigFileParser::fillHostVariablesMap()
+{
+	_hostVariables["name"] = HostVariables::NAME;
+	_hostVariables["root"] = HostVariables::ROOT;
+	_hostVariables["methods"] = HostVariables::METHODS;
+	_hostVariables["default_index"] = HostVariables::DEFAULT_INDEX;
+	_hostVariables["default_error"] = HostVariables::DEFAULT_ERROR;
+}
+
+void ConfigFileParser::fillServerVariablesMap()
+{
+	_serverVariables["listen_port"] = ServerVariables::LISTEN_PORT;
+	_serverVariables["buffer_size"] = ServerVariables::BUFFER_SIZE;
+	_serverVariables["mime_config"] = ServerVariables::MIME_CONFIG;
+	_serverVariables["listen_backlog"] = ServerVariables::LISTEN_BACKLOG;
+}
+
+void ConfigFileParser::parseServerVariable(const std::string &line, ServerConfig &config) const
+{
+	size_t pos = std::min(line.find('\t'), line.find(' '));
+	if (pos == std::string::npos)
+	{
+		ERROR("No value assigned to variable on line [" << _lineCount << "]: " << line);
+		throw std::runtime_error("No value for variable");
+	}
+	std::string key = line.substr(0, pos);
+	std::string value = line.substr(pos + 1, line.size() - pos);
+	value = removeLeadingWhitespace(value);
+
+	std::map<std::string, int>::const_iterator result = _serverVariables.find(key);
+	if (result == _serverVariables.end())
+	{
+		ERROR("Invalid server-variable encountered on line [" << _lineCount << "]: " << line);
+		throw std::runtime_error("Invalid variable encountered");
+	}
+
+	switch (result->second)
+	{
+	case ServerVariables::LISTEN_PORT:
+		parseVariable(value, &config.port);
+		break;
+	case ServerVariables::BUFFER_SIZE:
+		parseVariable(value, &config.buffer_size);
+		break;
+	case ServerVariables::MIME_CONFIG:
+		parseVariable(value, &config.mimeTypesFile);
+		break;
+	case ServerVariables::LISTEN_BACKLOG:
+		parseVariable(value, &config.listen_backlog);
+		break;
+	}
+}
+
+void ConfigFileParser::parseHostVariable(const std::string &line, HostConfig &config) const
+{
+	size_t pos = std::min(line.find('\t'), line.find(' '));
+	if (pos == std::string::npos)
+	{
+		ERROR("No value assigned to variable on line [" << _lineCount << "]: " << line);
+		throw std::runtime_error("No value for variable");
+	}
+	std::string key = line.substr(0, pos);
+	std::string value = line.substr(pos + 1, line.size() - pos);
+	value = removeLeadingWhitespace(value);
+
+	std::map<std::string, int>::const_iterator result = _hostVariables.find(key);
+	if (result == _hostVariables.end())
+	{
+		ERROR("Invalid host-variable encountered on line [" << _lineCount << "]: " << line);
+		throw std::runtime_error("Invalid variable encountered");
+	}
+
+	switch (result->second)
+	{
+	case HostVariables::NAME:
+		parseVariable(value, &config.names);
+		break;
+	case HostVariables::ROOT:
+		parseVariable(value, &config.root);
+		break;
+	case HostVariables::METHODS:
+		parseVariable(value, &config.acceptedMethods);
+		break;
+	case HostVariables::DEFAULT_INDEX:
+		parseVariable(value, &config.defaultIndex);
+		break;
+	case HostVariables::DEFAULT_ERROR:
+		parseVariable(value, &config.defaultError);
+		break;
+	}
 }
