28f933f31a1c6410308a61269a0f2df96cf4663e
Ryno Meiboom <rmeiboom@f1r6s3.codam.nl>
2022-05-10 17:48:58 +0200

    added basic handeling for 404 on a get

--- srcs/GETMethod.cpp
+++ srcs/GETMethod.cpp
@@ -10,6 +10,8 @@ GETMethod::~GETMethod()
 
 std::string createFilePath(std::string path)
 {
+	if (path.find("root") != std::string::npos)
+		return path.substr(1, path.size());
 	path.insert(0, "root");
 	return path;
 }
@@ -17,7 +19,6 @@ std::string createFilePath(std::string path)
 Response* GETMethod::process()
 {
 	std::ifstream* stream = new std::ifstream();
-
 	std::string target = _request.getTarget();
 	if (target == "/")
 		target = "/index.html"; // home returned of redirect naar home
@@ -25,13 +26,16 @@ Response* GETMethod::process()
 	stream->open(createFilePath(target));
 	if (stream->fail())
 	{
-		delete stream;
+		stream->close();
 		_response->setStatusCode(HttpStatusCodes::NOT_FOUND);
-		return _response;
+		stream->open(createFilePath("/error.html"));
+		if (!stream->is_open())
+			throw std::runtime_error("No error page");
 	}
+	else
+		_response->setStatusCode(HttpStatusCodes::OK);
 
 	_response->setBodyStream(stream);
-	_response->setStatusCode(HttpStatusCodes::OK);
 
 	return _response;
 }
