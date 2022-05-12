eeeda17773d6cb32814eb1eae19f1390c8565369
Ryno95 <rmeiboom@student.codam.nl>
2022-05-12 17:09:01 +0200

    Added general OkResponseClass

---
+++ srcs/OkStatusResponse.cpp
@@ -0,0 +1,36 @@
+#include <OkStatusResponse.hpp>
+#include <config/GlobalConfig.hpp>
+#include <Utility.hpp>
+
+namespace Webserver
+{
+    OkStatusResponse::OkStatusResponse() : Response(HttpStatusCodes::OK)
+    {
+        buildHeader();
+    }
+
+    OkStatusResponse::OkStatusResponse(std::ifstream* stream) : Response(HttpStatusCodes::OK)
+    {
+       setBodyStream(stream);
+       buildHeader();
+    }
+    
+    OkStatusResponse::~OkStatusResponse()
+    {
+        
+    }
+    
+    void OkStatusResponse::buildHeader()
+    {
+        addHeaderField("Server", SERVER_NAME);
+        addHeaderField("Range", "bytes");
+        addHeaderField("Date", getTimeStamp());
+        if (_bodyStream != nullptr)
+        {
+            getBodyStream()->seekg(0, std::ios_base::end);
+		    addHeaderField("Content-Length", std::to_string(_bodyStream->tellg()));
+            // std::string mimeType = GlobalConfig::get().mimeTypes()
+        // addHeaderField("Content-Type: ", images/jpeg); How to dynamically get content type???
+        }
+    }
+}
