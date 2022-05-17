6ef63980666199d55df29ffd689e2f87971042f7
Ryno95 <rmeiboom@student.codam.nl>
2022-05-17 16:52:20 +0200

    added modular badResponesTemplates

--- includes/responses/BadStatusResponse.hpp
+++ includes/responses/BadStatusResponse.hpp
@@ -7,12 +7,11 @@ namespace Webserver
 	class BadStatusResponse: public Response
 	{
 		public:
-			BadStatusResponse();
+			BadStatusResponse(HttpStatusCode code);
 			BadStatusResponse(HttpStatusCode code, const std::string &errorPage);
 			~BadStatusResponse();
 
 		private:
-		   void buildHeader(const std::string &errorPage);
 	};
 }
 
--- srcs/responses/BadStatusResponse.cpp
+++ srcs/responses/BadStatusResponse.cpp
@@ -7,36 +7,20 @@
 
 namespace Webserver
 {
-	BadStatusResponse::BadStatusResponse() : Response()
+	BadStatusResponse::BadStatusResponse(HttpStatusCode code) : Response(code)
 	{
-		buildHeader("");
 	}
 
 	BadStatusResponse::BadStatusResponse(HttpStatusCode code, const std::string &errorPage) : Response(code)
 	{
-		_bodyStream = new std::ifstream(errorPage);
+
+		setBodyStream(new std::ifstream(errorPage));
 		if (!_bodyStream->is_open())
 			throw std::runtime_error("Errorfile doens't exist");
-		buildHeader(errorPage);
+		createContentHeaders(errorPage);
 	}
 	
 	BadStatusResponse::~BadStatusResponse()
 	{
-	}
-	
-	// add safety for finlastfo
-	void BadStatusResponse::buildHeader(const std::string &errorPage)
-	{
-		addHeaderField("Server", SERVER_NAME);
-		addHeaderField("Range", "bytes");
-		addHeaderField("Date", getTimeStamp());
-		if (_bodyStream != nullptr && errorPage.size() > 1)
-		{
-			getBodyStream()->seekg(0, std::ios_base::end);
-			addHeaderField("Content-Length", std::to_string(_bodyStream->tellg()));
-			std::string fileExtensionWithoutDot = errorPage.substr(errorPage.find_last_of(".") + 1, errorPage.size());
-			std::string mimeType = GlobalConfig::get().mimeTypes.getMIMEType(fileExtensionWithoutDot);
-			addHeaderField("Content-Type", mimeType);
-		}
-	}
+	}	
 }
