5f6ecd4433c6c45a7802040f08bddc3a50eb92b0
pspijkst <pspijkst@student.codam.nl>
2022-05-19 13:39:43 +0200

    Tests use the Host class instead of (deprecated and removed) Router class

--- unit_tests/routeTests.cpp
+++ unit_tests/routeTests.cpp
@@ -4,7 +4,7 @@
 #include "../includes/config/ServerConfig.hpp"
 #include "../includes/config/HostConfig.hpp"
 #include "../includes/config/ConfigFileParser.hpp"
-#include "../includes/Router.hpp"
+#include "../includes/Host.hpp"
 
 #include <vector>
 #include <iostream>
@@ -15,47 +15,45 @@ ConfigFileParser parser("unit_tests/default.config");
 
 Test(RouteTests, NonExistingHost)
 {
-	std::vector<ServerConfig> configs = parser.parse();
-	Router router(configs[0]);
-	HostConfig host = router.getHost("asdfasdf", "/");
-	cr_expect(host.names[0] == "main_server.nl");
-	cr_expect(host.names[1] == "main_server.com");
-	cr_expect(host.root == "root");
+	std::vector<ServerConfig> config = parser.parse();
+	Host host = Host::determine(config[0], "asdfasdf", "/");
+
+	cr_expect(host.getName() == "main_server.nl");
+	cr_expect(host.getRoot() == "root");
 }
 
 Test(RouteTests, SpecificDefaultHost)
 {
-	std::vector<ServerConfig> configs = parser.parse();
-	Router router(configs[0]);
-	HostConfig host = router.getHost("main_server.nl", "/upload/images/123.png");
-	cr_expect(host.names[0] == "main_server.nl");
-	cr_expect(host.names[1] == "main_server.com");
-	cr_expect(host.root == "/data/upload/");
+	std::vector<ServerConfig> config = parser.parse();
+	Host host = Host::determine(config[0], "main_server.nl", "/upload/images/123.png");
+
+	cr_expect(host.getName() == "main_server.nl");
+	cr_expect(host.getRoot() == "/data/upload/");
 }
 
 Test(RouteTests, SpecificHostReroute)
 {
-	std::vector<ServerConfig> configs = parser.parse();
-	Router router(configs[0]);
-	HostConfig host = router.getHost("cgi_handler", "/root/cgi-bin/add.py");
-	cr_expect(host.names[0] == "cgi_handler");
-	cr_expect(host.root == "/root/cgi/");
+	std::vector<ServerConfig> config = parser.parse();
+	Host host = Host::determine(config[0], "cgi_handler", "/root/cgi-bin/add.py");
+
+	cr_expect(host.getName() == "cgi_handler");
+	cr_expect(host.getRoot() == "/root/cgi/");
 }
 
 Test(RouteTests, SpecificHostRedirect)
 {
-	std::vector<ServerConfig> configs = parser.parse();
-	Router router(configs[0]);
-	HostConfig host = router.getHost("cgi_handler", "/googlesearch/search=animals&kind=cats");
-	cr_expect(host.names[0] == "cgi_handler");
-	cr_expect(host.root == "www.google.com");
+	std::vector<ServerConfig> config = parser.parse();
+	Host host = Host::determine(config[0], "cgi_handler", "/googlesearch/search=animals&kind=cats");
+
+	cr_expect(host.getName() == "cgi_handler");
+	cr_expect(host.getRoot() == "www.google.com");
 }
 
 Test(RouteTests, SpecificHostUpload)
 {
-	std::vector<ServerConfig> configs = parser.parse();
-	Router router(configs[0]);
-	HostConfig host = router.getHost("cgi_handler", "/images/cool_images/sprite.jpg");
-	cr_expect(host.names[0] == "cgi_handler");
-	cr_expect(host.root == "/root/data/images");
+	std::vector<ServerConfig> config = parser.parse();
+	Host host = Host::determine(config[0], "cgi_handler", "/images/cool_images/sprite.jpg");
+
+	cr_expect(host.getName() == "cgi_handler");
+	cr_expect(host.getRoot() == "/root/data/images");
 }
