e48325cbb444dbd9f787cafed789d09a03cfe9b1
pspijkst <pspijkst@student.codam.nl>
2022-05-24 19:19:29 +0200

    Make use of the request's Connection header, by setting the response to 'close' or 'keep-alive' accordingly

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -36,6 +36,7 @@ namespace Webserver
 	{
 		try
 		{
+			hasCommunicated();
 			recvRequests();
 			processRequests();
 		}
@@ -48,8 +49,12 @@ namespace Webserver
 
 	void Client::writeHandler()
 	{
+		if (_needsRemove == true)
+			return;
+
 		try
 		{
+			hasCommunicated();
 			sendResponses();
 		}
 		catch(const DisconnectedException& e)
@@ -115,14 +120,16 @@ namespace Webserver
 
 			// an error occured during parsing / preparing the request, so we send the error-code back
 			if (request.getStatus() != HttpStatusCodes::OK)
-			{
 				response = new BadStatusResponse(request.getStatus());
-				DEBUG("Invalid request received.");
-			}
 			else
-			{
 				response = processValidRequest(request);
-			}
+
+			// after we created a new response, we also need to communicate "Connection: close" if the client requested that from us.
+			std::string connectionValue;
+			if (request.tryGetHeader(Header::Connection, connectionValue) && stringToLower(connectionValue) == "close")
+				response->addHeader(Header::Connection, "close");
+			else
+				response->addHeader(Header::Connection, "keep-alive");
 
 			_requestQueue.pop_front();
 			if (response != nullptr) // If we ALWAYS want to respond, we could send BAD_REQUEST
@@ -137,7 +144,13 @@ namespace Webserver
 			if (_responseQueue.size() > 0) // If we have some queued, pop the front
 			{
 				Response* response = _responseQueue.front();
+
 				// Check connection header to know if we should close after sending this response
+				std::string connectionValue;
+				if (response->tryGetHeader(Header::Connection, connectionValue) && stringToLower(connectionValue) == "close")
+					_closeAfterRespond = true;
+				else
+					_closeAfterRespond = false;
 
 				_sender.setResponse(response);
 				_responseQueue.pop_front();
@@ -150,6 +163,9 @@ namespace Webserver
 		{ // otherwise we can deactivate POLLOUT, since there's nothing prepared for us...
 			PollHandler::setWriteEnabled(_fd, false);
 		}
+
+		if (_sender.hasResponse() == false && _closeAfterRespond == true)
+			_needsRemove = true;
 	}
 
 	/*
@@ -170,6 +186,9 @@ namespace Webserver
 	void Client::checkTimeout(timeval now)
 	{
 		if (((now.tv_sec - _lastCommunicated.tv_sec) * 1000) + ((now.tv_usec - _lastCommunicated.tv_usec) / 1000) >= TIMEOUT_MS)
+		{
 			_needsRemove = true;
+			DEBUG("Connection timed-out fd: " << _fd);
+		}
 	}
 }
