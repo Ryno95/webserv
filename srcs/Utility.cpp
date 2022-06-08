d8a71c0cc5958db3a943afd813d0f6c564e69589
Ryno95 <rmeiboom@student.codam.nl>
2022-06-08 15:54:56 +0200

    refactored Prepend

--- srcs/Utility.cpp
+++ srcs/Utility.cpp
@@ -93,14 +93,14 @@ namespace Webserver
 	std::string prependRoot(const std::string& root, const std::string& target)
 	{
 		const size_t	targetSlashPos = target.find('/');
-		const size_t	rootSlashPos = root.find('/');
+		const size_t	rootSlashPos = root.find_last_of('/');
 		const size_t	rootSize = root.size();
 		const size_t	targetSize = target.size();
 
-		if (rootSlashPos == rootSize && targetSlashPos == targetSize)
+		if (rootSlashPos == rootSize - 1 && targetSlashPos == 0)
 			return std::string(root + target.substr(targetSlashPos + 1, target.size()));
-		else if ((rootSlashPos == rootSize && targetSlashPos != 0)
-					|| (rootSlashPos != rootSize && targetSlashPos == 0))
+		else if ((rootSlashPos == rootSize - 1 && targetSlashPos != 0)
+					|| (rootSlashPos != rootSize - 1 && targetSlashPos == 0))
 			return std::string(root + target);
 		else
 			return std::string(root + "/" + target);
