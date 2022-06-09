a253fdc00d08e11fc7a77c1ad1bd533eb9ebd6e4
Ryno95 <rmeiboom@student.codam.nl>
2022-06-09 12:26:47 +0200

    Basic functionality of delete implemented

--- srcs/methods/DELETEMethod.cpp
+++ srcs/methods/DELETEMethod.cpp
@@ -1,5 +1,19 @@
-#include <methods/DELETEMethod.hpp>
+#include <Utility.hpp>
+#include <sys/stat.h>
 
+#include <HttpStatusCode.hpp>
+#include <Logger.hpp>
+#include <methods/DELETEMethod.hpp>
+#include <responses/BadStatusResponse.hpp>
+#include <responses/OkStatusResponse.hpp>
+
+
+//	If a DELETE method is successfully applied, the origin server SHOULD
+//	send a 202 (Accepted) status code if the action will likely succeed
+//	but has not yet been enacted, a 204 (No Content) status code if the
+//	action has been enacted and no further information is to be supplied,
+//	or a 200 (OK) status code if the action has been enacted and the
+//	response message includes a representation describing the status.
 namespace Webserver
 {
 	DELETEMethod::DELETEMethod(const Request &request, const Host& host) : AMethod(request, host)
@@ -10,10 +24,20 @@ namespace Webserver
 	{
 	}
 
+
 	Response* DELETEMethod::process()
 	{
-		WARN("DELETE is not yet implemented!");
+		// WARN("DELETE is not yet implemented!");
+		const std::string fullTarget = prependRoot(_host.getRoot(), _request.getTarget());
+		struct stat fileInfo;
 
-		return nullptr;
+		if (stat(fullTarget.c_str(), &fileInfo) == SYSTEM_ERR)
+			return new BadStatusResponse(HttpStatusCodes::NOT_FOUND);
+		else if (fileInfo.st_mode != S_IWUSR | fileInfo.st_mode != S_IXUSR) // check that file has write/exec permissions
+			return new BadStatusResponse(HttpStatusCodes::FORBIDDEN);
+		else if (remove(fullTarget.c_str()) == SYSTEM_ERR)
+			return new BadStatusResponse(HttpStatusCodes::INTERNAL_ERROR);
+
+		return new OkStatusResponse(HttpStatusCodes::OK);
 	}
 }
