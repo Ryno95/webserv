992637858bdfae65070a7d819a4fe63fd8897f4a
Ryno95 <rmeiboom@student.codam.nl>
2022-06-09 12:35:54 +0200

    added check for ifMethod is allowed

--- srcs/methods/DELETEMethod.cpp
+++ srcs/methods/DELETEMethod.cpp
@@ -27,13 +27,13 @@ namespace Webserver
 
 	Response* DELETEMethod::process()
 	{
-		// WARN("DELETE is not yet implemented!");
-		const std::string fullTarget = prependRoot(_host.getRoot(), _request.getTarget());
-		struct stat fileInfo;
+		const std::string 	fullTarget = prependRoot(_host.getRoot(), _request.getTarget());
+		const bool			isMethodAllowed = _host.isMethodAccepted(this->_request.getMethod());
+		struct stat 		fileInfo;
 
 		if (stat(fullTarget.c_str(), &fileInfo) == SYSTEM_ERR)
 			return new BadStatusResponse(HttpStatusCodes::NOT_FOUND);
-		else if (fileInfo.st_mode != S_IWUSR | fileInfo.st_mode != S_IXUSR) // check that file has write/exec permissions
+		else if (!isMethodAllowed || fileInfo.st_mode != S_IWUSR || fileInfo.st_mode != S_IXUSR) // check that file has write/exec permissions
 			return new BadStatusResponse(HttpStatusCodes::FORBIDDEN);
 		else if (remove(fullTarget.c_str()) == SYSTEM_ERR)
 			return new BadStatusResponse(HttpStatusCodes::INTERNAL_ERROR);
