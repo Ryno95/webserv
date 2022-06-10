cba11f74763bd2df8b18de09d4d3f9427b6a8ffd
Pascal Spijkstra <pascalspijkstra@gmail.com>
2022-06-10 09:17:28 +0200

    remove commented code

--- includes/Host.hpp
+++ includes/Host.hpp
@@ -7,17 +7,11 @@
 
 namespace Webserver
 {
-	struct Host : public HostConfig
+	struct Host : public LocationConfig
 	{
 		static Host		determine(const ServerConfig& config, const std::string& hostName, const std::string& uri);
 
 		std::string		getName() const;
-
-		// bool			isMethodAccepted(Method::method) const;
-		// bool			isRedirect() const;
-		// bool			isUpload() const;
-		// bool			isChangeRoot() const;
-		// bool			isCgi() const;
 
 	private:
 		static const HostConfig& matchHost(const std::vector<HostConfig>& hosts, const std::string& hostName);
