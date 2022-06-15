39f96f087a129663520d1723ecb57df26beeb15c
Ryno95 <rmeiboom@student.codam.nl>
2022-06-15 14:21:07 +0200

    refactored acc tests

--- acceptance_tests/PyTests/integratedMethods.py
+++ acceptance_tests/PyTests/integratedMethods.py
@@ -8,9 +8,12 @@ import os
 from defines import Colors, returnStatus, Methods, HttpResponseStatus, LOCAL_HOST
 from Request import Request, GETRequest, POSTRequest ,DELETERequest, IntegrateMethods
 
+
+# IntegrateMethods(target, expectedGetResponse):
+# IntgratedMethods all expect the same return code, so only good actually for OK responses
+# Target should NOT start with a forward slash as appending happens internally
 EXIT_CODE = 0
 
-# IntegrateMethods(target, expectedGetResponse)
 target = "simplePost.txt"
 im = IntegrateMethods(target, HttpResponseStatus.OK)
 EXIT_CODE += im.runMethods()
