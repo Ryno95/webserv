0cec98ee1cb183765d172a2189eb0d4d68b67d35
pspijkst <pspijkst@student.codam.nl>
2022-06-21 15:34:32 +0200

    Config parsing updated, and updated the tests accordingly

--- unit_tests/routeTests.cpp
+++ unit_tests/routeTests.cpp
@@ -1,9 +1,11 @@
 #include <criterion/criterion.h>
 
-#include "../includes/config/LocationConfig.hpp"
-#include "../includes/config/ServerConfig.hpp"
-#include "../includes/config/HostConfig.hpp"
-#include "../includes/config/ConfigFileParser.hpp"
+// #include "../includes/config/LocationConfig.hpp"
+// #include "../includes/config/ServerConfig.hpp"
+// #include "../includes/config/HostConfig.hpp"
+#include "../includes/config/Parser.hpp"
+#include "../includes/config/AppConfig.hpp"
+#include "../includes/config/AppConfigParser.hpp"
 #include "../includes/Host.hpp"
 
 #include <vector>
@@ -11,49 +13,48 @@
 
 using namespace Webserver;
 
-ConfigFileParser parser("unit_tests/default.config");
 
 Test(RouteTests, NonExistingHost)
 {
-	std::vector<ServerConfig> config = parser.parse();
-	Host host = Host::determine(config[0], "asdfasdf", "/");
+	std::ifstream stream("unit_tests/default.config");
+	AppConfig* config = Parser<AppConfigParser>(stream).parse();
+	Host host = Host::determine(*config->getChildren()[0], "asdfasdf", "/");
 
-	cr_expect(host.getName() == "main_server.nl");
 	cr_expect(host.getRoot() == "root");
 }
 
-Test(RouteTests, SpecificDefaultHost)
+Test(RouteTests, UploadLocation)
 {
-	std::vector<ServerConfig> config = parser.parse();
-	Host host = Host::determine(config[0], "main_server.nl", "/upload/images/123.png");
+	std::ifstream stream("unit_tests/default.config");
+	AppConfig* config = Parser<AppConfigParser>(stream).parse();
+	Host host = Host::determine(*config->getChildren()[0], "main_server.nl", "/upload/images/123.png");
 
-	cr_expect(host.getName() == "main_server.nl");
-	cr_expect(host.getRoot() == "/data/upload/");
+	cr_expect(host.getRoot() == "root/data/");
 }
 
 Test(RouteTests, SpecificHostReroute)
 {
-	std::vector<ServerConfig> config = parser.parse();
-	Host host = Host::determine(config[0], "cgi_handler", "/root/cgi-bin/add.py");
+	std::ifstream stream("unit_tests/default.config");
+	AppConfig* config = Parser<AppConfigParser>(stream).parse();
+	Host host = Host::determine(*config->getChildren()[0], "cgi_handler", "/root/cgi-bin/add.py");
 
-	cr_expect(host.getName() == "cgi_handler");
-	cr_expect(host.getRoot() == "/root/cgi/");
+	cr_expect(host.getRoot() == "root/cgi-bin/");
 }
 
 Test(RouteTests, SpecificHostRedirect)
 {
-	std::vector<ServerConfig> config = parser.parse();
-	Host host = Host::determine(config[0], "cgi_handler", "/googlesearch/search=animals&kind=cats");
+	std::ifstream stream("unit_tests/default.config");
+	AppConfig* config = Parser<AppConfigParser>(stream).parse();
+	Host host = Host::determine(*config->getChildren()[0], "google", "/googlesearch/search=animals&kind=cats");
 
-	cr_expect(host.getName() == "cgi_handler");
 	cr_expect(host.getRoot() == "www.google.com");
 }
 
 Test(RouteTests, SpecificHostUpload)
 {
-	std::vector<ServerConfig> config = parser.parse();
-	Host host = Host::determine(config[0], "cgi_handler", "/images/cool_images/sprite.jpg");
+	std::ifstream stream("unit_tests/default.config");
+	AppConfig* config = Parser<AppConfigParser>(stream).parse();
+	Host host = Host::determine(*config->getChildren()[0], "cgi_handler", "/images/cool_images/sprite.jpg");
 
-	cr_expect(host.getName() == "cgi_handler");
-	cr_expect(host.getRoot() == "/root/data/images");
+	cr_expect(host.getRoot() == "root/data/");
 }
