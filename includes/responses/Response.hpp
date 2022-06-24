6829476179c7c57100aa8117d03ecc092285fe64
Pascal Spijkstra <pascalspijkstra@gmail.com>
2022-06-24 12:36:31 +0200

    AResponse back to Response class, which supports setting statuscodes and adding files

---
+++ includes/responses/Response.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <fstream>
+#include <map>
+#include <string>
+#include <sstream>
+
+#include <HttpStatusCode.hpp>
+#include <defines.hpp>
+#include <Utility.hpp>
+#include <HeaderFields.hpp>
+
+namespace Webserver
+{
+	#define NotFoundErrorPage "root/NotFoundErrorPage.html"
+	#define BadRequestErrorPage "root/BadRequestErrorPage.html"
+
+	class Response : public HeaderFields
+	{
+	public:
+		Response();
+		Response(const Response &ref);
+		Response(HttpStatusCode code);
+		Response &operator=(const Response &rhs);
+		virtual ~Response();
+
+		virtual std::istream *getBodyStream() const;
+
+		void			addFile(const std::string& filePath);
+		std::istream	*getHeaderStream();
+		HttpStatusCode	getStatusCode() const;
+		void			setStatusCode(HttpStatusCode code);
+
+	protected:
+		void setBodyStream(std::istream* fileStream);
+
+		HttpStatusCode		_statusCode;
+
+	private:
+		void createBodyHeaders(const std::string &fileName);
+		void addConstantHeaderFields();
+
+		std::istream*		_bodyStream;
+		std::stringstream	_headerStream;
+	};
+}
---
+++ srcs/responses/Response.cpp
@@ -0,0 +1,121 @@
+#include <fstream>
+#include <iostream>
+
+#include <Logger.hpp>
+#include <responses/Response.hpp>
+#include <Utility.hpp>
+#include <Exception.hpp>
+#include <Webserv.hpp>
+
+namespace Webserver
+{
+	Response::Response() :
+		_statusCode(HttpStatusCodes::OK),
+		_bodyStream(nullptr)
+	{
+	}
+
+	Response::Response(HttpStatusCode code) :
+		_statusCode(code),
+		_bodyStream(nullptr)
+	{
+		addConstantHeaderFields();
+	}
+
+	Response::Response(const Response &ref)
+	{
+		*this =	ref;
+	}
+
+	Response::~Response()
+	{
+		if (_bodyStream != nullptr)
+			delete _bodyStream;
+	}
+
+	Response& Response::operator=(const Response &rhs)
+	{
+		this->_statusCode = rhs._statusCode;
+		return (*this);
+	}
+
+	void Response::addConstantHeaderFields()
+	{
+		addHeader(Header::Server, SERVER_NAME);
+		addHeader(Header::AcceptRanges, "bytes");
+		addHeader(Header::Date, getTimeStamp());
+	}
+
+	static std::string getContentTypeHeader(const std::string &fileName)
+	{
+		const size_t	extensionIndex = fileName.find_last_of(".");
+		std::string		fileExtensionWithoutDot;
+		std::string 	mimeType;
+
+		if (extensionIndex != std::string::npos)
+			fileExtensionWithoutDot = fileName.substr(extensionIndex + 1, std::string::npos);
+
+		mimeType = Webserv::config().getMimeTypes().getMIMEType(fileExtensionWithoutDot);
+
+		return mimeType;
+	}
+
+	std::istream *Response::getHeaderStream()
+	{
+		std::map<std::string, std::string>::const_iterator cursor = headersBegin();
+		std::map<std::string, std::string>::const_iterator end = headersEnd();
+		
+		std::string	header = HTTPVERSION;
+		header += " ";
+		header += std::to_string(_statusCode.first);
+		header += " ";
+		header += _statusCode.second;
+		while (cursor != end)
+		{
+			header += "\r\n";
+			header += cursor->first;
+			header += ": ";
+			header += cursor->second;
+			++cursor;
+		}
+		header += "\r\n\r\n";
+		_headerStream << header;
+		return &_headerStream;
+	}
+
+	void Response::addFile(const std::string& filePath)
+	{
+		std::ifstream* stream = new std::ifstream(filePath);
+		setBodyStream(stream);
+
+		if (!stream->is_open())
+			throw InvalidRequestException(HttpStatusCodes::NOT_FOUND);
+
+		createBodyHeaders(filePath); // probably not OK yet, because this is the full file path and not just the file name.
+	}
+
+	void Response::setBodyStream(std::istream* stream)
+	{
+		if (_bodyStream != nullptr)
+			delete _bodyStream;
+
+		_bodyStream = stream;
+	}
+
+	void Response::createBodyHeaders(const std::string &fileName)
+	{
+		getBodyStream()->seekg(0, std::ios_base::end);
+		addHeader(Header::ContentLength, std::to_string(_bodyStream->tellg()));
+		addHeader(Header::ContentType, getContentTypeHeader(fileName));
+	}
+
+	std::istream* Response::getBodyStream() const
+	{
+		return _bodyStream;
+	}
+
+	void Response::setStatusCode(HttpStatusCode code)
+	{
+		_statusCode = code;
+	}
+}
