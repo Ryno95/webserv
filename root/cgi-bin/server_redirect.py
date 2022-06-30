73b0d1f6c6999ad2d4dead7c2c58afda6cd21c5b
pspijkst <pspijkst@student.codam.nl>
2022-06-30 17:26:22 +0200

    Example cgi script to return internal server_redirect

---
+++ root/cgi-bin/server_redirect.py
@@ -0,0 +1,8 @@
+print("Status: 200 OK")
+print("Location: /Users/pspijkst/Webserv/root/server_redirect.html\n")
+print("<!DOCTYPE html>\n<html>")
+
+print("<h1>Server redirect</h1>")
+print("<h2>This should not be printed</h2>")
+
+print("</html>")
