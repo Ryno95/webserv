a416d97d712fe162b1b8fdfa731287c86852b362
pspijkst <pspijkst@student.codam.nl>
2022-08-02 13:43:18 +0200

    Read until finished instead of waiting with usleep(), and more descriptive print messages.

--- tests/acceptance_tests/pipeline_client.cpp
+++ tests/acceptance_tests/pipeline_client.cpp
@@ -15,7 +15,7 @@ std::string makeGetRequest(char const* target)
 	std::string buffer;
 
 	buffer += "GET ";
-	buffer += std::string(target);
+	buffer += target;
 	buffer += " HTTP/1.1\r\n";
 	buffer += "host: pipeline client\r\n";
 	buffer += "\r\n";
@@ -25,13 +25,23 @@ std::string makeGetRequest(char const* target)
 void sendRequest(std::string request)
 {
 	send(sockFd, request.c_str(), request.size(), 0);
-	std::cout << "Sent: " << request << std::endl;
 }
 
 void readRequests()
 {
-	// bytesRead = read( sockFd , buffer, 1024);
-	// printf("%s\n",buffer );
+	char buffer[1024];
+	while (1)
+	{
+		int bytes = read(sockFd , buffer, 1023);
+		if (bytes > 0)
+		{
+			buffer[bytes] = '\0';
+			std::cout << buffer << std::endl;
+		}
+		else
+			break;
+	}
+	std::cout << "Done receiving responses." << std::endl;
 }
 
 int main(int argc, char const *argv[])
@@ -50,7 +60,7 @@ int main(int argc, char const *argv[])
 
 	if ((sockFd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		printf("\n Socket creation error \n");
+		perror("Socket error");
 		return -1;
 	}
 
@@ -60,19 +70,20 @@ int main(int argc, char const *argv[])
 	// Convert IPv4 and IPv6 addresses from text to binary form
 	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
 	{
-		printf("\nInvalid address/ Address not supported \n");
+		perror("Invalid address");
 		return -1;
 	}
 
 	if (connect(sockFd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 	{
-		printf("\nConnection Failed \n");
+		perror("Connection failed");
 		return -1;
 	}
 
 	for (size_t i = 0; i < CONNECTION_COUNT; i++)
 	{
-		sendRequest(makeGetRequest(std::to_string(i).c_str()));
+		sendRequest(makeGetRequest("index.html"));
 	}
-	usleep(std::min(std::max(100000 * CONNECTION_COUNT, 1000000), 2000000));
+	std::cout << "Done sending " << CONNECTION_COUNT << " requests, waiting for responses..." << std::endl << std::endl;
+	readRequests();
 }
